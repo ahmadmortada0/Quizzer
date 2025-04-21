e889b1af3afc56f787e675bfa1d1e31d4656e189
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-04-21 12:49:08 +0300

    changing <> coloring the table

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,20 +1,24 @@
 let users= JSON.parse(localStorage.getItem("usersAndScores"))
-console.log(users)
+
+let table = document.getElementById("table-id");
+let rowId = 0;
+
 for (let i = 0; i < users.length; i++) {
-  let id =0
-    for (let j = 0; j < users[i].userScores.length; j++) {
-         document.getElementById("table-id").innerHTML +=`
-            <tr>
-              <td class="">${users[i].username}</td>
-              <td class="">${users[i].userEmail}</td>
-              <td class="">${users[i].userScores[j].scoreTitle}</td>
-              <td class="">${users[i].userScores[j].totalscore}</td>
-            </tr>
-`
-id++;
-}
-if (id%2===0){
-  let q = document.querySelectorAll("td")
-  q.classList.add(".color")
-}
+  for (let j = 0; j < users[i].userScores.length; j++) {
+    let row = document.createElement("tr");
+
+    if (rowId % 2 === 0) {
+      row.classList.add("color");
+    }
+
+    row.innerHTML = `
+      <td class="name">${users[i].username}</td>
+      <td class="email">${users[i].userEmail}</td>
+      <td class="scoreT">${users[i].userScores[j].scoreTitle}</td>
+      <td class="score">${users[i].userScores[j].totalscore}</td>
+    `;
+
+    table.appendChild(row);
+    rowId++;
+  }
 }
