4f0a84fff45c036ceca9495806064525dea35319
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-04-21 22:49:46 +0300

    creating <> a function that change the form value and make some inputs visible

--- scripts/login.js
+++ scripts/login.js
@@ -23,3 +23,15 @@ function logIN(){
  login=true
 
 }
+
+//a function that change the form value and make some inputs visible
+
+function signIn(){
+  form.innerHTML="Sign in"
+  document.getElementById("sgn-btn").classList.add("active-btn")
+  document.getElementById("lgn-btn").classList.remove("active-btn")
+  document.getElementById("con-confirm-in").classList.remove("invisible")
+  document.getElementById("con-in-name").classList.remove("invisible")
+  submitBtn.innerHTML="Sign in"
+  login=false
+}
